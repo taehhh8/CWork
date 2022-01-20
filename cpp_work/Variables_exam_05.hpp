d96c414eb4c7dfce6d53973a352214508b5bd239
taehhh8 <taehyun8534@gmail.com>
2022-01-20 14:30:07 +0900

    tae
    1/20일

--- cpp_work/Variables_exam_05.hpp
+++ cpp_work/Variables_exam_05.hpp
@@ -1,5 +1,4 @@
 
-
 #define WIDTH 1024
 #define HEIGHT 768
 #define NEW_LINE "\n";
---
+++ cpp_work/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+; //줄을 안바꾸고 cin받기
+    cin >> x; 
+    cout << "Yout Input Number : " << x << endl;
+
+    return 0;
+}
